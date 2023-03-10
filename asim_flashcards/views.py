ca48b4c5243da6fd0a2e37c6aafb9f94ffdeae5b
aravk <aravkasi@gmail.com>
2023-03-09 19:35:48 -0600

    Use pdm instead of poetry, and add CSS file

--- asim_flashcards/views.py
+++ asim_flashcards/views.py
@@ -1,6 +1,12 @@
 from asim_flashcards import app
-from flask import render_template
+from flask import render_template, send_from_directory
+
 
 @app.route('/')
 def get_index():
     return render_template('index.html', name='test value')
+
+
+@app.route('/styles/<path:path>')
+def send_styles(path):
+    return send_from_directory('../static/styles', path)
