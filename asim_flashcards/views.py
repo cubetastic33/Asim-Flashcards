df9d756f6e080c2c91b7b1f68634c77a2053258d
aravk <aravkasi@gmail.com>
2023-04-12 14:11:27 -0500

    Update paths to work through flask

--- asim_flashcards/views.py
+++ asim_flashcards/views.py
@@ -7,6 +7,21 @@ def get_index():
     return render_template('index.html', name='test value')
 
 
+@app.route('/home')
+def get_home():
+    return render_template('home.html', name='test value')
+
+
+@app.route('/start')
+def get_start():
+    return render_template('start.html', name='test value')
+
+
+@app.route('/flashcards')
+def get_flashcards():
+    return render_template('flashcards.html', name='test value')
+
+
 @app.route('/styles/<path:path>')
 def send_styles(path):
     return send_from_directory('../static/styles', path)
