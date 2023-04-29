bff77a54d4e347c987d04938c4a50945e1b93afc
aravk <aravkasi@gmail.com>
2023-04-28 19:22:31 -0500

    Move scripts to static directory

--- asim_flashcards/views.py
+++ asim_flashcards/views.py
@@ -7,37 +7,17 @@ from flask import render_template, send_from_directory, send_file
 def get_index():
     return render_template('home.html', name='test value')
 
-@app.route('/home.js')
-def get_homejs():
-    return render_template('home.js')
-
 @app.route('/navbar')
 def get_navbar():
     return render_template('navbar.html', name='test value')
-
-@app.route('/navbar.js')
-def get_navbarjs():
-    return render_template('navbar.js')
-
-@app.route('/timer.js')
-def get_timerjs():
-    return render_template('timer.js')
 
 @app.route('/start')
 def get_start():
     return render_template('start.html', name='test value')
 
-@app.route('/start.js')
-def get_startjs():
-    return render_template('start.js', name='test value')
-
 @app.route('/calendar')
 def get_calendar():
     return render_template('calendar.html', name='test value')
-
-@app.route('/calendar.js')
-def get_calendarjs():
-    return render_template('calendar.js')
 
 @app.route('/flashcards')
 def get_flashcards():
@@ -59,26 +39,22 @@ def get_signup():
 def get_study():
     return render_template('study.html', name='test value')
 
-@app.route('/study.js')
-def get_studyjs():
-    return render_template('study.js', name='test value')
-
 @app.route('/get_sound')
 def get_sound():
     return send_file('templates/complete.wav', mimetype='audio/wav')
+
+@app.route('/settings')
+def get_settings():
+    return render_template('settings.html', name='test value')
 
 @app.route('/styles/<path:path>')
 def send_styles(path):
     return send_from_directory('../static/styles', path)
 
-@app.route('/settings')
-def get_setting():
-    return render_template('settings.html', name='test value')
+@app.route('/scripts/<path:path>')
+def send_js(path):
+    return send_from_directory('../static/scripts', path)
 
 @app.route('/images/<path:path>')
 def send_images(path):
     return send_from_directory('../static/images', path)
-
-@app.route('/js/<path:path>')
-def send_js(path):
-    return send_from_directory('../static/js', path)
