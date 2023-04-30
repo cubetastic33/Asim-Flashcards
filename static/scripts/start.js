e4e00ed314f217f7a532f9ea77709e225314fb55
aravk <aravkasi@gmail.com>
2023-04-30 09:25:14 -0500

    Reformat file

--- static/scripts/start.js
+++ static/scripts/start.js
@@ -9,7 +9,7 @@ function navigateToQuiz(deckId) {
 (async () => {
 
   // Get the modal
-  const modal = document.getElementById("add-flashcard-modal");
+  const modal = document.getElementById('add-flashcard-modal');
 
   // Append each flashcard to the container element
   const container = document.getElementById('flashcard-container');
@@ -18,9 +18,9 @@ function navigateToQuiz(deckId) {
   addCardContainer.className = 'flashcard';
   container.appendChild(addCardContainer);
 
-  addCardContainer.addEventListener('click', function() {
-      modal.style.display = 'block';
-    });
+  addCardContainer.addEventListener('click', function () {
+    modal.style.display = 'block';
+  });
 
   // Create plus sign
   const plusSign = document.createElement('div');
@@ -41,11 +41,11 @@ function navigateToQuiz(deckId) {
   // Replace this array with code that fetches flashcards from a database
   var cards;
   await fetch('/get_flashcards?' + new URLSearchParams({
-    name: deckId
-})).then((response) => response.json())
-.then((responseJson) => {
-  cards = responseJson;
-});
+    name: deckId,
+  })).then((response) => response.json())
+    .then((responseJson) => {
+      cards = responseJson;
+    });
   /*
   const cards = [
     { front: 'What is an automaton?' },
@@ -61,25 +61,25 @@ function navigateToQuiz(deckId) {
     const flashcard = document.createElement('div');
     flashcard.className = 'flashcard';
     flashcard.textContent = card.front;
-    
-    const trashCanBackground = document.createElement("div");
-    trashCanBackground.className = "trash-icon-background";
 
-    const trashCanIcon = document.createElement("span");
-    trashCanIcon.className = "trash-icon";
+    const trashCanBackground = document.createElement('div');
+    trashCanBackground.className = 'trash-icon-background';
+
+    const trashCanIcon = document.createElement('span');
+    trashCanIcon.className = 'trash-icon';
     trashCanIcon.innerHTML = '<i class="fa fa-trash"></i>';
-    trashCanIcon.setAttribute("data-card-id", card.id);
+    trashCanIcon.setAttribute('data-card-id', card.id);
 
-    trashCanIcon.addEventListener("click", async (event) => {
+    trashCanIcon.addEventListener('click', async (event) => {
       event.stopPropagation(); // Prevent the click event from propagating to the card element
-      const confirmed = confirm("Are you sure you want to delete this flashcard?");
+      const confirmed = confirm('Are you sure you want to delete this flashcard?');
       if (confirmed) {
-        const cardId = event.target.getAttribute("data-card-id");
+        const cardId = event.target.getAttribute('data-card-id');
         const formData = new FormData();
-        formData.append("card_id", cardId)
+        formData.append('card_id', cardId);
         await fetch('/delete_flashcard', {
           method: 'POST',
-          body: formData
+          body: formData,
         })
           .then((response) => {
             if (!response.ok) {
@@ -94,7 +94,7 @@ function navigateToQuiz(deckId) {
           });
       }
     });
-    
+
     trashCanBackground.appendChild(trashCanIcon);
     flashcard.appendChild(trashCanBackground);
 
@@ -102,77 +102,75 @@ function navigateToQuiz(deckId) {
   }
 
   // Get the <span> element that closes the modal
-  const closeBtn = document.getElementsByClassName("close")[0];
+  const closeBtn = document.getElementsByClassName('close')[0];
 
   // Get the button that opens the modal
-  const addCardBtn = document.getElementById("add-flashcard-btn");
+  const addCardBtn = document.getElementById('add-flashcard-btn');
 
-  addCardBtn.onclick = async function() {
-      const frontCard = document.querySelector("#front").value;
-      const backCard = document.querySelector("#back").value;
-    
-      if (!frontCard || !backCard) {
-        // Display an error message if either #front or #back is empty.
-        const errorMessage = document.createElement("div");
-        errorMessage.textContent = "Please fill in both front and back fields.";
-        errorMessage.className = "error";
-        document.body.appendChild(errorMessage);
-    
-        // Fade out the error message after a few seconds.
-        setTimeout(() => {
-          errorMessage.style.opacity = "0";
-        }, 2000);
-    
-        // Remove the error message after the fade-out effect is complete.
-        setTimeout(() => {
-          document.body.removeChild(errorMessage);
-        }, 3000);
-    
-        return;
-      }
-    
-      // This is where you could add the front and back to the database.
-      const data = new FormData();
-      data.append("name", deckId)
-      data.append("front", frontCard)
-      data.append("back", backCard)
-      await fetch('/create_flashcard', {
-        method: 'POST',
-        body: data
-      })
-    
-      // Display a confirmation message.
-      const confirmation = document.createElement("div");
-      confirmation.textContent = "Card created successfully!";
-      confirmation.className = "confirmation";
-      document.body.appendChild(confirmation);
-    
-      // Fade out the confirmation message after a few seconds.
+  addCardBtn.onclick = async function () {
+    const frontCard = document.querySelector('#front').value;
+    const backCard = document.querySelector('#back').value;
+
+    if (!frontCard || !backCard) {
+      // Display an error message if either #front or #back is empty.
+      const errorMessage = document.createElement('div');
+      errorMessage.textContent = 'Please fill in both front and back fields.';
+      errorMessage.className = 'error';
+      document.body.appendChild(errorMessage);
+
+      // Fade out the error message after a few seconds.
       setTimeout(() => {
-        confirmation.style.opacity = "0";
+        errorMessage.style.opacity = '0';
       }, 2000);
-    
-      // Remove the confirmation message after the fade-out effect is complete.
-      setTimeout(() => {
-        document.body.removeChild(confirmation);
-      }, 3000);
-    
-      // Clear the #front and #back input fields.
-      document.querySelector("#front").value = "";
-      document.querySelector("#back").value = "";
-    };
-    
-    
-    
 
-  closeBtn.onclick = function() {
-    modal.style.display = "none";
-  }
+      // Remove the error message after the fade-out effect is complete.
+      setTimeout(() => {
+        document.body.removeChild(errorMessage);
+      }, 3000);
+
+      return;
+    }
+
+    // This is where you could add the front and back to the database.
+    const data = new FormData();
+    data.append('name', deckId);
+    data.append('front', frontCard);
+    data.append('back', backCard);
+    await fetch('/create_flashcard', {
+      method: 'POST',
+      body: data,
+    });
+
+    // Display a confirmation message.
+    const confirmation = document.createElement('div');
+    confirmation.textContent = 'Card created successfully!';
+    confirmation.className = 'confirmation';
+    document.body.appendChild(confirmation);
+
+    // Fade out the confirmation message after a few seconds.
+    setTimeout(() => {
+      confirmation.style.opacity = '0';
+    }, 2000);
+
+    // Remove the confirmation message after the fade-out effect is complete.
+    setTimeout(() => {
+      document.body.removeChild(confirmation);
+    }, 3000);
+
+    // Clear the #front and #back input fields.
+    document.querySelector('#front').value = '';
+    document.querySelector('#back').value = '';
+  };
+
+
+  closeBtn.onclick = function () {
+    modal.style.display = 'none';
+  };
 
   // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
+  window.onclick = function (event) {
     if (event.target == modal) {
-      modal.style.display = "none";
+      modal.style.display = 'none';
     }
-  }
+  };
 })();
